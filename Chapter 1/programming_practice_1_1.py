2e4d811e5b56ec9c37b1968f7ab8ba35bb3bdf97
David Szczepanik <david.szczepanik@outlook.com>
2025-09-03 20:55:53 -0500

    programming_practice_1_1

---
+++ Chapter 1/programming_practice_1_1.py
@@ -0,0 +1,23 @@
+# Printing the poem Song of the Open Road
+# Author: David Szczepanik
+
+print("Song of the Open Road, by Walt Whitman\n")
+
+print("Afoot and light-hearted I take to the open road,")
+print("Healthy, free, the world before me,")
+print("The long brown path before me leading wherever I choose.\n")
+
+print("Henceforth I ask not good-fortune, I myself am good-fortune,")
+print("Henceforth I whimper no more, postpone no more, need nothing,")
+print("Done with indoor complaints, libraries, querulous criticisms,")
+print("Strong and content I travel the open road.\n")
+
+print("The earth, that is sufficient,")
+print("I do not want the constellations any nearer,")
+print("I know they are very well where they are,")
+print("I know they suffice for those who belong to them.\n")
+
+print("(Still here I carry my old delicious burdens,")
+print("I carry them, men and women, I carry them with me wherever I go,")
+print("I swear it is impossible for me to get rid of them,")
+print("I am fill’d with them, and I will fill them in return.)\n")
